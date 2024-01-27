ba0f2d3be361702aa2946f5239ae119e53ff8a12
Vitor Gabriel <102386982+vitingr@users.noreply.github.com>
2024-01-27 10:35:50 -0300

    Add files via upload

--- go.mod
+++ go.mod
@@ -8,16 +8,24 @@ require (
 	github.com/antchfx/htmlquery v1.3.0 // indirect
 	github.com/antchfx/xmlquery v1.3.18 // indirect
 	github.com/antchfx/xpath v1.2.4 // indirect
+	github.com/chromedp/cdproto v0.0.0-20240127002248-bd7a66284627 // indirect
+	github.com/chromedp/chromedp v0.9.3 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
 	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.3.2 // indirect
 	github.com/gocolly/colly v1.2.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/gorilla/handlers v1.5.2 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
@@ -29,6 +37,7 @@ require (
 	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/protobuf v1.26.0 // indirect
--- main.go
+++ main.go
@@ -6,8 +6,11 @@ import (
 	"log"
 	"os"
 	"strings"
+	"context"
 
 	"github.com/gocolly/colly"
+	"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/chromedp"
 
 	"net/http"
 
@@ -55,8 +58,6 @@ func handleSearchSupermarketData(w http.ResponseWriter, r *http.Request) {
 
 func getSearchData(searchTerm string) []item {
 	var items []item
-
-	fmt.Println(searchTerm)
 
 	callPagueMenos := colly.NewCollector(
 		colly.AllowedDomains("https://atacadao.com.br/catalogo", "www.atacadao.com.br/catalogo", "https://www.superpaguemenos.com.br/", "www.superpaguemenos.com.br"),
@@ -127,6 +128,71 @@ func getSupermarketData() []item {
 
 	var items []item
 
+	// Vai gerar uma context diferente para cada website a ser acessado pelo chromedp
+	saoVicenteContext, cancel := chromedp.NewContext(context.Background())
+	pavanContext, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	// Scrape the DIV content from web js rendered websites
+	var saoVicenteNodes []*cdp.Node
+	var pavanContext []*cdp.Node
+
+	// Vai carregar o código renderizado via javascript do website
+	chromedp.Run(saoVicenteContext,
+		chromedp.Navigate("https://www.sitemercado.com.br/supermercadossaovicente/santa-barbara-d-oeste-loja-sao-vicente-centro-sbo-centro-graca-martins"),
+		chromedp.Nodes(".list-product-item", &saoVicenteNodes, chromedp.ByQueryAll),
+	)
+
+	for _, node := range saoVicenteNodes {
+
+		var photo, name, price string
+
+		// Vai realizar um laço de repetição para cada produto da loja
+		chromedp.Run(saoVicenteContext,
+			chromedp.AttributeValue("img", "src", &photo, nil, chromedp.ByQuery, chromedp.FromNode(node)),
+			chromedp.AttributeValue(".list-product-link", "aria-label", &name, nil, chromedp.ByQuery, chromedp.FromNode(node)),
+			chromedp.Text(".preco-oferta", &price, chromedp.ByQuery, chromedp.FromNode(node)),
+		)
+
+		// Geração do item e adição do mesmo no objeto de produtos
+		product := item{
+			Unidade: "São Vicente"
+			Photo: photo,
+			Title: name,
+			Price: price,
+		}
+
+		items = append(items, product)
+	}
+
+	// Vai carregar o código renderizado via javascript do website
+	chromedp.Run(pavanContext,
+		chromedp.Navigate("https://www.sitemercado.com.br/supermercadospavan/santa-barbara-d-oeste-loja-jardim-perola-jardim-esmeralda-av-do-comercio"),
+		chromedp.Nodes(".list-product-item", &pavanContext, chromedp.ByQueryAll),
+	)
+
+	for _, node := range pavanContext {
+
+		var photo, name, price string
+
+		// Vai realizar um laço de repetição para cada produto da loja
+		chromedp.Run(pavanContext,
+			chromedp.AttributeValue("img", "src", &photo, nil, chromedp.ByQuery, chromedp.FromNode(node)),
+			chromedp.AttributeValue(".list-product-link", "aria-label", &name, nil, chromedp.ByQuery, chromedp.FromNode(node)),
+			chromedp.Text(".preco-oferta", &price, chromedp.ByQuery, chromedp.FromNode(node)),
+		)
+
+		// Geração do item e adição do mesmo no objeto de produtos
+		product := item{
+			Unidade: "Pavan",
+			Photo: photo,
+			Title: name,
+			Price: price,
+		}
+
+		items = append(items, product)
+	}
+
 	callPagueMenos := colly.NewCollector(
 		colly.AllowedDomains("https://atacadao.com.br/catalogo", "www.atacadao.com.br/catalogo", "https://www.superpaguemenos.com.br/", "www.superpaguemenos.com.br"),
 	)
@@ -195,6 +261,7 @@ func main() {
 	// Configuração de Rotas
 	router := mux.NewRouter()
 
+	// Configurações dos middlewares de conexão da API
 	corsMiddleware := handlers.CORS(
 		handlers.AllowedOrigins([]string{"*"}),
 		handlers.AllowedMethods([]string{"*"}),
