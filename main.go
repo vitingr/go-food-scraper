e96289cce71e8937f0f7376ef7f66bf458e0ddd6
Vitor Gabriel <102386982+vitingr@users.noreply.github.com>
2024-01-27 11:52:19 -0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"strings"
-	"context"
 	"time"
 
-	"github.com/gocolly/colly"
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
+	"github.com/gocolly/colly"
 
 	"net/http"
 
@@ -59,6 +59,46 @@ func handleSearchSupermarketData(w http.ResponseWriter, r *http.Request) {
 
 func getSearchData(searchTerm string) []item {
 	var items []item
+
+	// Vai gerar uma context diferente para cada website a ser acessado pelo chromedp
+	saoVicenteContext, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	// Scrape the DIV content from web js rendered websites
+	var saoVicenteNodes []*cdp.Node
+
+	// Vai carregar o código renderizado via javascript do website
+	chromedp.Run(saoVicenteContext,
+		chromedp.Navigate("https://www.sitemercado.com.br/supermercadossaovicente/santa-barbara-d-oeste-loja-sao-vicente-centro-sbo-centro-graca-martins"),
+		chromedp.Sleep(10*time.Second),
+		chromedp.Nodes(".list-product-item", &saoVicenteNodes, chromedp.ByQueryAll),
+	)
+
+	for _, node := range saoVicenteNodes {
+
+		var photo, name, price string
+
+		// Vai realizar um laço de repetição para cada produto da loja
+		chromedp.Run(saoVicenteContext,
+			chromedp.AttributeValue("img", "src", &photo, nil, chromedp.ByQuery, chromedp.FromNode(node)),
+			chromedp.AttributeValue(".list-product-link", "aria-label", &name, nil, chromedp.ByQuery, chromedp.FromNode(node)),
+			chromedp.Text(".preco-oferta", &price, chromedp.ByQuery, chromedp.FromNode(node)),
+		)
+
+		fmt.Println("https://www.sitemercado.com.br/supermercadossaovicente/santa-barbara-d-oeste-loja-sao-vicente-centro-sbo-centro-graca-martins")
+
+		// Geração do item e adição do mesmo no objeto de produtos
+		item := item{
+			Unidade: "São Vicente",
+			Photo:   photo,
+			Title:   name,
+			Price:   price,
+		}
+
+		if strings.Contains(strings.ToLower(item.Title), strings.ToLower(searchTerm)) {
+			items = append(items, item)
+		}
+	}
 
 	callPagueMenos := colly.NewCollector(
 		colly.AllowedDomains("https://atacadao.com.br/catalogo", "www.atacadao.com.br/catalogo", "https://www.superpaguemenos.com.br/", "www.superpaguemenos.com.br"),
@@ -131,12 +171,10 @@ func getSupermarketData() []item {
 
 	// Vai gerar uma context diferente para cada website a ser acessado pelo chromedp
 	saoVicenteContext, cancel := chromedp.NewContext(context.Background())
-	// pavanContext, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 
 	// Scrape the DIV content from web js rendered websites
 	var saoVicenteNodes []*cdp.Node
-	// var pavanNodes []*cdp.Node
 
 	// Vai carregar o código renderizado via javascript do website
 	chromedp.Run(saoVicenteContext,
@@ -156,44 +194,18 @@ func getSupermarketData() []item {
 			chromedp.Text(".preco-oferta", &price, chromedp.ByQuery, chromedp.FromNode(node)),
 		)
 
+		fmt.Println("https://www.sitemercado.com.br/supermercadossaovicente/santa-barbara-d-oeste-loja-sao-vicente-centro-sbo-centro-graca-martins")
+
 		// Geração do item e adição do mesmo no objeto de produtos
-		product := item{
+		item := item{
 			Unidade: "São Vicente",
-			Photo: photo,
-			Title: name,
-			Price: price,
+			Photo:   photo,
+			Title:   name,
+			Price:   price,
 		}
 
-		items = append(items, product)
+		items = append(items, item)
 	}
-
-	// Vai carregar o código renderizado via javascript do website
-	// chromedp.Run(pavanContext,
-	// 	chromedp.Navigate("https://www.sitemercado.com.br/supermercadospavan/santa-barbara-d-oeste-loja-jardim-perola-jardim-esmeralda-av-do-comercio"),
-	// 	chromedp.Nodes(".list-product-item", &pavanNodes, chromedp.ByQueryAll),
-	// )
-
-	// for _, node := range pavanNodes {
-
-	// 	var photo, name, price string
-
-	// 	// Vai realizar um laço de repetição para cada produto da loja
-	// 	chromedp.Run(pavanContext,
-	// 		chromedp.AttributeValue("img", "src", &photo, nil, chromedp.ByQuery, chromedp.FromNode(node)),
-	// 		chromedp.AttributeValue(".list-product-link", "aria-label", &name, nil, chromedp.ByQuery, chromedp.FromNode(node)),
-	// 		chromedp.Text(".preco-oferta", &price, chromedp.ByQuery, chromedp.FromNode(node)),
-	// 	)
-
-	// 	// Geração do item e adição do mesmo no objeto de produtos
-	// 	product := item{
-	// 		Unidade: "Pavan",
-	// 		Photo: photo,
-	// 		Title: name,
-	// 		Price: price,
-	// 	}
-
-	// 	items = append(items, product)
-	// }
 
 	callPagueMenos := colly.NewCollector(
 		colly.AllowedDomains("https://atacadao.com.br/catalogo", "www.atacadao.com.br/catalogo", "https://www.superpaguemenos.com.br/", "www.superpaguemenos.com.br"),
